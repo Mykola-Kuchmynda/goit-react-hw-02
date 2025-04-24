0d38834e6d63dbc8afdd15a759d8eff02bf55cb0
Mykola Kuchmynda <mykola.kuchmynda@gmail.com>
2025-04-24 17:38:36 +0300

    start

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Description from '../Description/Description'
+
+//import css from './App.module.css'
+
+export default function App() {
+  return (
+    <>
+  <Description />
+    </>
+  )
+}
+
+
